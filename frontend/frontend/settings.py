54dd9f07f4f855efb5474eb61de772b7caddb053
orionempire <orionempire@gmail.com>
2012-09-12 15:30:49 -0400

    local config started

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -9,16 +9,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': '',                      # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -117,8 +117,10 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'admin_gui',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -149,3 +151,13 @@ LOGGING = {
         },
     }
 }
+
+#load local settings
+import sys
+try:
+   local_settings_path = '/etc/linux-admin-toolkit/config_files/frontend/'
+   if local_settings_path not in sys.path:
+       sys.path.append(local_settings_path)
+   from local_settings import *
+except ImportError:
+   pass
